8185b552397a0e4e8ccf3db9bdc4027e437f440d
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-04-19 23:52:25 +0800

    Delete constants.js

--- Constants/constants.js
+++
@@ -1,5 +0,0 @@
-﻿export const _loginController = 'loginController';
-export const _daskboardController = 'daskboardController';
-export const _dataBindingController = 'dataBindingController';
-export const _loadApiController = 'loadApiController';
-export const _mainPageController = 'mainPageController';
