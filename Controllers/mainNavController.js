da8edf1116cdfea7fa72c770c76ed775e54488ba
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-04-20 00:11:32 +0800

    Add files via upload

---
+++ Controllers/mainNavController.js
@@ -0,0 +1,21 @@
+﻿
+var app = angular
+    .module("App", ['ui.router', 'ngStorage'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/login");
+        $stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: 'Templates/login.html',
+                controller: "loginController",
+
+            })
+            
+
+      
+    })
+    
+  
+    
+    
+   
