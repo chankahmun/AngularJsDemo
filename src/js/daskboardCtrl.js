55197d21a6f9f941d91456b24f168a0713fefe41
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-04-23 14:58:28 +0800

    Add files via upload

--- src/js/daskboardCtrl.js
+++ src/js/daskboardCtrl.js
@@ -3,36 +3,65 @@
 angular.module("App").controller('daskBCtrl', daskBoardFunction)
 
 function daskBoardFunction($http, $sessionStorage, $scope) {
-    /*
-    var onCallSetting = function (){
-        $http.get("../../api.json").then(function(response){
-            $sessionStorage.financialApiUrl = response.data.api;
-            $sessionStorage.financialApiKey = response.data.key;
-            
-        }).then(function(){
-            calling($sessionStorage, $http)
-        })
+    
+    $scope.limitDisplay = 5;
+    $scope.decimalPlaces = 2;
+    $scope.priceStyle = function (price) {
+
+        var style;
+
+        if (price > 0) {
+            style = { "color": "green" }
+        } else {
+            style = { "color": "red" }
+        }
+
+        return style
     }
-    */
+
     var onCallNewsApi = function (data, status, headers, config){
 
         $sessionStorage.financialApiUrl = data.data.data.api;
         $sessionStorage.financialApiKey = data.data.data.key;
-        
-        $http.get($sessionStorage.financialApiUrl + "/api/v3/available-traded/list?apikey=" + $sessionStorage.financialApiKey)
-        .then(function (response) {
-            console.log("url:", $sessionStorage.financialApiUrl + "/api/v3/available-traded/list?apikey=" + $sessionStorage.financialApiKey)
-            $scope.tradelist = response['data'];
-            console.log("pasdsaage loaaa")
-        });
+
+        getGainer($http, $sessionStorage, $scope);
+        getLoser($http, $sessionStorage, $scope);
+        getTradedLst($http, $sessionStorage, $scope);
         
     }
 
+   
+
     var promise = $http.get("../../api.json");
     promise.then(onCallNewsApi);
+ 
+}
 
-    
-   
+function getTradedLst($http, $sessionStorage, $scope){
+    $http.get($sessionStorage.financialApiUrl + "/api/v3/available-traded/list?apikey=" + $sessionStorage.financialApiKey)
+    .then(function (response) {
+       
+        $scope.tradelist = response['data'];
+        
+    });
+}
+
+function getLoser($http, $sessionStorage, $scope) {
+    $http.get($sessionStorage.financialApiUrl + "/api/v3/losers?apikey=" + $sessionStorage.financialApiKey)
+        .then(function (response) {
+
+            $scope.toplosers = response['data'];
+
+        });
+}
+
+function getGainer($http, $sessionStorage, $scope) {
+    $http.get($sessionStorage.financialApiUrl + "/api/v3/gainers?apikey=" + $sessionStorage.financialApiKey)
+        .then(function (response) {
+
+            $scope.topgainers = response['data'];
+
+        });
 }
 
 
