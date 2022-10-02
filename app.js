e5adc8e32c26cacc2760d5ada9e2d2d87bf8547c
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-10-02 16:14:48 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var myApp = angular.module('App', ['ui.router', 'ngStorage']);
 
-myApp.config(function($stateProvider,$urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-  $stateProvider.state({
-    name: 'login',
-    url: '/login',
-    templateUrl: 'src/templates/login.html',
-    controller: "loginCtrl"
-  });
+myApp.config(function($stateProvider,$urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/daskboad/main');
+  // $stateProvider.state({
+  //   name: 'login',
+  //   url: '/login',
+  //   templateUrl: 'src/templates/login.html',
+  //   controller: "loginCtrl"
+  // });
 
   $stateProvider
   .state('daskb',{
-    url: '/',
+     abstract: true,
+    url: '/daskboad',
     templateUrl: 'src/templates/daskboard.html',
     controller: "daskBCtrl"
   })
@@ -19,9 +20,30 @@ myApp.config(function($stateProvider,$urlRouterProvider) {
     url:'/main',
     templateUrl:'src/templates/mainpage.html',
     controller: 'mainCtrl'
+  })
+  .state('daskb.watchlists',{
+    url:'/watchlists',
+    templateUrl:'src/templates/watchlists.html',
+    controller: 'watchlistsCtrl'
+  })
+  .state('notfound',{
+    url:'/notfound',
+    templateUrl:'src/templates/watchlists.html',
+    controller: 'watchlistsCtrl'
   });
+  
 
+  
+
+//  $locationProvider.html5Mode(true);
+$locationProvider.html5Mode({
+  enabled: true,
+  requireBase: true
+});
 
   
 })
+
+
+
 
